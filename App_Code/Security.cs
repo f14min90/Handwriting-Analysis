21872c083d3cece545158a94138b4d9f809c0230
f14min90 <45079234+f14min90@users.noreply.github.com>
2019-08-05 01:22:12 +0300

    Add files via upload

---
+++ App_Code/Security.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+
+public class Security
+{
+    public static string Encrypt(string content)
+    {
+        SHA256 sha = SHA256.Create();
+        byte[] bytes = Encoding.UTF8.GetBytes(content);
+        byte[] hash = sha.ComputeHash(bytes);
+
+        string result = "";
+        foreach (byte b in hash)
+            result += b.ToString("x2");
+
+        return result;
+    }
+
+    public static string Check_cookie(string cookie_value)
+    {
+        string cookie_hash = Encrypt(cookie_value);
+        string expression = "select Email from users where CookieHash=@cookie_hash";
+
+        using (SqlConnection connection = new SqlConnection(Service.connectionString))
+        {
+            SqlCommand command = new SqlCommand(expression, connection);
+            command.Parameters.AddWithValue("@cookie_hash", cookie_hash);
+
+            connection.Open();
+            return command.ExecuteScalar()?.ToString();
+        }
+    }
+}
---
+++ App_Code/Server.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public enum ResponseType
+{
+    OK,
+    ERROR,
+    VALUE,
+    IMAGES
+}
+
+public struct ServerResponse
+{
+    public ResponseType type;
+    public double value;
+    public List<UserImage> list;
+}
